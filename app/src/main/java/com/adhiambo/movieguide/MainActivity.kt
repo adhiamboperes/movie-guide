653a25fc1a70743acdbf897b8989c7db0ca50982
Adhiambo Peres <adhiambooyier@gmail.com>
2022-09-15 15:41:44 +0300

    Add navigation component
    Set up application navgraph and data binding, to define app launcher activity and entry point

---
+++ app/src/main/java/com/adhiambo/movieguide/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.adhiambo.movieguide
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.adhiambo.movieguide.databinding.ContentMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ContentMainBinding
+
+    private val navController by lazy { findNavController(R.id.nav_host_fragment) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.content_main)
+        binding.lifecycleOwner = this
+
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        val bottomNavigationView = binding.bottomNavBar
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_movies -> navController.navigate(R.id.movieListFragment)
+            }
+            true
+        }
+    }
+}
---
+++ app/src/main/java/com/adhiambo/movieguide/MovieListFragment.kt
@@ -0,0 +1,7 @@
+package com.adhiambo.movieguide
+
+import androidx.fragment.app.Fragment
+
+class MovieListFragment: Fragment(R.layout.fragment_movie_list) {
+
+}
